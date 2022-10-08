bd17f8277f1445f3a452aac96ee1bb3c5597fe6c
FlySwat23 <104340408+TylerSwatman@users.noreply.github.com>
2022-10-08 18:44:02 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -1,4 +1,15 @@
-function temperatureConverter(valNum) {
-  valNum = parseFloat(valNum);
-  document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+function gcd_two_numbers(x, y) {
+  if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    return false;
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
 }
+
+console.log(gcd_two_numbers(12, 13));
+console.log(gcd_two_numbers(9, 3));
