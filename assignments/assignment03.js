c4ca37580cf3811446cdbbd94710c548a79fa221
FlySwat23 <104340408+TylerSwatman@users.noreply.github.com>
2022-10-09 17:46:49 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -21,7 +21,7 @@ for (let i = 1; i <= number1 && i <= number2; i++) {
 
     // check if is factor of both integers
     if( number1 % i == 0 && number2 % i == 0) {
-        hcf = i;
+        gcd = i;
     }
 }
 
