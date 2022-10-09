4521da06214c3b5ebf1a92169c495cd50b16b340
FlySwat23 <104340408+TylerSwatman@users.noreply.github.com>
2022-10-09 18:25:18 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -13,9 +13,9 @@ for (let i = 1; i <= numberOne && i <= numberTwo; i++) {
    
     if( numberOne % i == 0 && numberTwo % i == 0) {
         gcd = i;
-      
+       document.getElementById("outputGCD").innerHTML=gcd;
     }
 }
-output("GCD of ${number1} and ${number2} is ${gcd}.");
+
 
 
