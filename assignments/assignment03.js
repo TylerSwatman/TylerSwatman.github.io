d9424fc286599f1f4219c8da31cf8212617072c5
FlySwat23 <104340408+TylerSwatman@users.noreply.github.com>
2022-10-09 18:35:08 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -13,8 +13,10 @@ for (let i = 1; i <= numberOne && i <= numberTwo; i++) {
    
     if( numberOne % i == 0 && numberTwo % i == 0) {
         gcd = i;
-       document.getElementById("outputGCD").innerHTML=gcd;
     }
+}
+       document.getElementById("outputGCD").innerHTML=gcd;
+    
 }
 
 
