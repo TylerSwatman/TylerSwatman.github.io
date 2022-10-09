f4802e58f439c481c51d545f38b67dbd2291e3df
FlySwat23 <104340408+TylerSwatman@users.noreply.github.com>
2022-10-09 17:27:29 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -11,12 +11,19 @@ function gcd_two_numbers(x, y) {
   return x;
 }
 
-console.log(gcd_two_numbers(12, 13));
-console.log(gcd_two_numbers(9, 3));
+let gcd;
+// take input
+const number1 = prompt('Enter a first positive integer: ');
+const number2 = prompt('Enter a second positive integer: ');
 
-function add_two_digits(n)
-   {
-      return n % 10 + Math.floor(n / 10);
-   }
-console.log(add_two_digits(25))
-console.log(add_two_digits(50))
+// looping from 1 to number1 and number2
+for (let i = 1; i <= number1 && i <= number2; i++) {
+
+    // check if is factor of both integers
+    if( number1 % i == 0 && number2 % i == 0) {
+        hcf = i;
+    }
+}
+
+// display the hcf
+console.log("HCF of ${number1} and ${number2} is ${gcd}.");
