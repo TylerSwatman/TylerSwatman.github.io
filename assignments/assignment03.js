2e6ad6c45f99461c898f85c4d5b0ce1b9f849bf7
FlySwat23 <104340408+TylerSwatman@users.noreply.github.com>
2022-10-10 15:47:18 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -1,4 +1,3 @@
-
 let gcd;
 
 
@@ -21,4 +20,19 @@ for (let i = 1; i <= numberOne && i <= numberTwo; i++) {
    
        document.getElementById("outputGCD").innerHTML = gcd;
    
+}
+
+document.getElementById("myButtonTwo").addEventListener("click", myFunctionOne);
+
+function myFunctionOne(){
+  var num = document.getElementById("num").value;
+  num = parseFloat(num);
+  
+ let sum = 0;
+  while (num >0){
+    let rem = num % 10;
+    sum=sum + rem;
+    num = parseInt(num /10);
+  }
+  document.getElementById("outputSON").innerHTML = sum;
 }
