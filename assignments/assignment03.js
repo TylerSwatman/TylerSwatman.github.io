89dea70e27f7c777d5aca07d99804e9c9ffad427
FlySwat23 <104340408+TylerSwatman@users.noreply.github.com>
2022-10-11 00:06:08 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -51,7 +51,7 @@ const winConditions = [
     [2, 4, 6]
 ];
 let options = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
+let currentPlayer = "T";
 let running = false;
 
 initializeGame();
@@ -77,7 +77,7 @@ function updateCell(cell, index){
     cell.textContent = currentPlayer;
 }
 function changePlayer(){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    currentPlayer = (currentPlayer == "T") ? "S" : "T";
     statusText.textContent = `${currentPlayer}'s turn`;
 }
 function checkWinner(){
@@ -111,7 +111,7 @@ function checkWinner(){
     }
 }
 function restartGame(){
-    currentPlayer = "X";
+    currentPlayer = "T";
     options = ["", "", "", "", "", "", "", "", ""];
     statusText.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => cell.textContent = "");
