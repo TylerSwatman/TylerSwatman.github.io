31aee3c6ace5b24705d9bec40e83abaf5c899091
FlySwat23 <104340408+TylerSwatman@users.noreply.github.com>
2022-12-09 21:30:24 -0500

    Update assignment05.js

--- assignments/assignment05.js
+++ assignments/assignment05.js
@@ -1,5 +1,6 @@
 
 
+// default values
 let loans = [
   { loan_year: 2020, loan_amount: 10000.0, loan_int_rate: 0.0453 },
     { loan_year: 2021, loan_amount: 10000.0, loan_int_rate: 0.0453 },
@@ -12,22 +13,28 @@ let loanWithInterest = 0;
   let int = 0;
 let payments;
 
+// ----- JavaScript Functions -----
 
+// -------------------------------------------------------
+//adds commas in thousands value
 function toComma(value) {
   return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-
+// -------------------------------------------------------
+//adds dollar sign at the front of toComma
 let toMoney = (value) => {
   return   `\$${toComma(value.toFixed(2))}`;
 };
 
-
+// -------------------------------------------------------
+//it saves it in local storage of that device
 let saveForm = () => {
   localStorage.setItem(`as06`, JSON.stringify(loans));
 };
 
-
+// -------------------------------------------------------
+//gets inside the localstorage and updates the form in that localstorage
 let loadForm = () => {
   if (localStorage.getItem(`as06`) != null) {
     loans = JSON.parse(localStorage.getItem(`as06`));
@@ -40,15 +47,15 @@ let loadForm = () => {
 
 
 function loadDoc() {
-  
-  var defaultYear = loans[0].loan_year;
-    $("#loan_year0" + 1).val(defaultYear++); 
+  // pre-fill defaults for first loan year
+  var defaultYear = loans[0].loan_year; //pre filling the default year in the first year. Its declaring and initializing
+    $("#loan_year0" + 1).val(defaultYear++); //Jquery selector, Increments default year in the first line
   var defaultLoanAmount = loans[0].loan_amount;
-  $("#loan_amt0" + 1).val(defaultLoanAmount.toFixed(2)); 
+  $("#loan_amt0" + 1).val(defaultLoanAmount.toFixed(2)); //Jquery selector, setting default loan amount in first line
   var defaultInterestRate = loans[0].loan_int_rate;
-  $("#loan_int0" + 1).val(defaultInterestRate); 
+  $("#loan_int0" + 1).val(defaultInterestRate); //Jquery selector, setting default InterestRate in first line
    var loanWithInterest = loans[0].loan_amount * (1 + loans[0].loan_int_rate);
-  $("#loan_bal0" + 1).text(toMoney(loanWithInterest)); 
+  $("#loan_bal0" + 1).text(toMoney(loanWithInterest)); // Jquery selector, uses toMoney & setting default loanWithInterest in first line
 
   for (var i = 2; i < 6; i++) {
     $(`#loan_year0${i}`).val(defaultYear++); //increment year
@@ -56,7 +63,7 @@ function loadDoc() {
     $(`#loan_year0${i}`).css({
       backgroundColor: "grey",
       color: "white"
-    });
+    }); // makes it look disabled. makes background gray and white text
     $(`#loan_amt0${i}`).val(defaultLoanAmount.toFixed(2));
      $(`#loan_int0${i}`).val(defaultInterestRate);
     $(`#loan_int0${i}`).attr("disabled", "true");
@@ -64,17 +71,18 @@ function loadDoc() {
       backgroundColor: "grey",
       color: "white"
     });
-    loanWithInterest = 
+    loanWithInterest = //keeps updating the interest to have the total
       (loanWithInterest + defaultLoanAmount) * (1 + defaultInterestRate);
-    $("#loan_bal0" + i).text(toMoney(loanWithInterest));
-  } 
+    $("#loan_bal0" + i).text(toMoney(loanWithInterest)); //adds dollar signs and comma at balance
+  } // end: "for" loop
 
-  
+  //all input fields: select contents on focus
+  //highlight background yellow when clicked or in focus
   $("input[type=text]").focus(function () {
     $(this).select();
     $(this).css("background-color", "yellow");
   });
- 
+  //makes background white when it is deselected or in blur
   $("input[type=text]").blur(function () {
     $(this).css("background-color", "white");
     updateLoansArray();
@@ -83,33 +91,34 @@ function loadDoc() {
 
 } 
 
-
+// -------------------------------------------------------
 function updateLoansArray() {
- 
+  //takes user input and change the data in loans array using regex
 
-  let yearP = /^(19|20)\d{2}$/; 
-  let amtP = /^([1-9][0-9]*)+(.[0-9]{1,2})?$/; 
-  let intP = /^(0|)+(.[0-9]{1,5})?$/; 
+  let yearP = /^(19|20)\d{2}$/; //has to be 19 or 20 at the start and followed by two numbers
+  let amtP = /^([1-9][0-9]*)+(.[0-9]{1,2})?$/; //first digit has to start 1-9 and followed by none, one or more additional digits and then plus two or one decimal digit.
+  let intP = /^(0|)+(.[0-9]{1,5})?$/; //have zero at the start have 1 to 5 decimal places
   let valid = true;
 
+  //checks yearP matches the year pattern. background turns red if false
   if (!yearP.test($(`#loan_year01`).val())) {
     valid = false;
     $(`#loan_year01`).css("background-color", "red");
   }
-
+  //checks if amtP matches the ammount pattern. background turns red if false
   for (i = 1; i < 6; i++) {
     if (!amtP.test($(`#loan_amt0${i}`).val())) {
       valid = false;
       $(`#loan_amt0${i}`).css("background-color", "red");
     }
   }
-
+  //check if intP matches the interest rate pattern. background turns red if false
   if (!intP.test($(`#loan_int01`).val())) {
     valid = false;
     $(`#loan_int01`).css("background-color", "red");
   }
 
- 
+  //if users input are true then it updates the variables and form
   if (valid) {
     loans[0].loan_year = parseInt($("#loan_year01").val());
     for (var i = 1; i < 5; i++) {
@@ -124,10 +133,11 @@ function updateLoansArray() {
       loans[i].loan_int_rate = rate;
     }
 
-    updateForm(); 
+    updateForm(); // only updates if inputs are valid then it will update the form
   } 
 } 
 
+// -------------------------------------------------------
 
 let updateForm = () => {
   loanWithInterest = 0;
@@ -143,30 +153,31 @@ let updateForm = () => {
     $("#loan_bal0" + i).text(toMoney(loanWithInterest));
   }
   int = loanWithInterest - totalAmt;
-  $(`#loan_int_accrued`).text(toMoney(int)); 
-}; 
+  $(`#loan_int_accrued`).text(toMoney(int)); //show the interest
+}; // end: function updateForm()
 
 
-
+//creates variable app and assign value angular module
 var app = angular.module("myApp", []);
 
-
+//contoller takes the total amount borrowed and spread it in how many years
 app.controller("myCtrl", function ($scope) {
-  $scope.payments = []; 
+  $scope.payments = []; // controller connects with the view in HTML
   $scope.populate = function () {
-   
-    updateForm(); 
+    //populates the payments array
+
+    updateForm(); //updates form
 
     //payments
-    let total = loanWithInterest; 
+    let total = loanWithInterest; //calculate total
     let iRate = loans[0].loan_int_rate;
     let r = iRate / 12;
     let n = 11;
 
     let pay =
-      12 * (total / (((1 + r) ** (n * 12) - 1) / (r * (1 + r) ** (n * 12)))); 
+      12 * (total / (((1 + r) ** (n * 12) - 1) / (r * (1 + r) ** (n * 12)))); //formula to get computation with same payments for every period
     for (let i = 0; i < 10; i++) {
-    
+      //loop that generate the calculation as the period increases
       total -= pay;
       let int = total * iRate;
       $scope.payments[i] = {
@@ -176,7 +187,7 @@ app.controller("myCtrl", function ($scope) {
         ye: toMoney((total += int))
       };
     }
- 
+    //calculates the last payment with the last total balance
     $scope.payments[10] = {
       year: loans[4].loan_year + 11,
       payment: toMoney(total),
